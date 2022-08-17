a7f0699c5785eaa21ea5b1d1c06a96b6cecd36f9
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-08-18 02:38:26 +0900

    [UPDATE] 다크모드, 로그인 버튼 이미지 색상 모드에 따라 변경

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,6 +1,9 @@
 function setDarkMode() {
   const darkMode = document.querySelectorAll('.darkMode');
   const wrap = document.querySelector('#wrap');
+  const darkModeImg = darkModeBtn.querySelector('img');
+  const outBtn = document.querySelector('#outBtn');
+  const outBtnImg = outBtn.querySelector('img');
   const isDarkMode = localStorage.getItem('darkmode');
   if (isDarkMode === 'true') {
     darkMode.forEach((element) => {
@@ -9,6 +12,8 @@ function setDarkMode() {
       element.style.border = `1px solid rgba(255, 255, 255, 0.8);`;
     });
     wrap.style.background = `rgba(255, 255, 255, 0.3)`;
+    darkModeImg.src = './img/dark.png';
+    outBtnImg.src = './img/logout-dark.png';
     localStorage.setItem('darkmode', false);
   } else {
     darkMode.forEach((element) => {
@@ -17,6 +22,8 @@ function setDarkMode() {
       element.style.border = `1px solid rgba(0, 0, 0, 0.8);`;
     });
     wrap.style.background = `rgba(0, 0, 0, 0.3)`;
+    darkModeImg.src = './img/light.svg';
+    outBtnImg.src = './img/logout-light.png';
     localStorage.setItem('darkmode', true);
   }
 }
